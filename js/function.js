4e820086f18babb0fda01d0415aefe0abbab3ad9
lisakolts <crokodile.liza@gmail.com>
2024-02-18 21:44:19 +0300

    Исправление ошибок

--- js/function.js
+++ js/function.js
@@ -2,9 +2,9 @@
 // которую нужно проверить, и максимальную длину и возвращает true,
 // если строка
 // меньше или равна указанной длине, и false, если строка длиннее.
-let checkString = (string, numberOfSymbol) => {
-    return (string.length <= numberOfSymbol) ? true: false;
-}
+const checkString = (string, numberOfSymbol) => {
+  return (string.length <= numberOfSymbol) ? true : false;
+};
 //Проверка
 console.log(checkString('проверяемая строка', 20));
 console.log(checkString('проверяемая строка', 18));
@@ -14,25 +14,25 @@ console.log(checkString('проверяемая строка', 10));
 // Функция для проверки, является ли строка палиндромом.
 // Палиндром — это слово или фраза,
 // которые одинаково читаются и слева направо и справа налево.
-let checkPalindrom = (word) => {
+const checkPalindrom = (word) => {
   word = word.toUpperCase();
   word = word.replaceAll(' ', '');
-  let lengthWord = word.length;
-  let arrayChar = word.split('');
+  const lengthWord = word.length;
+  const arrayChar = word.split('');
   let check = 0;
 
-  for (let i = 0; i < Math.floor(lengthWord/2); i++) {
-    if (arrayChar[i] === arrayChar[lengthWord-1-i]) {
+  for (let i = 0; i < Math.floor(lengthWord / 2); i++) {
+    if (arrayChar[i] === arrayChar[lengthWord - 1 - i]) {
       check++;
     }
   }
 
-  if (check === Math.floor(lengthWord/2)) {
+  if (check === Math.floor(lengthWord / 2)) {
     return true;
   }
 
   return false;
-}
+};
 
 //Проверка
 console.log(checkPalindrom('топот'));
@@ -44,22 +44,22 @@ console.log(checkPalindrom('Лёша на полке клопа нашёл '));
 // Функция принимает строку, извлекает содержащиеся в
 // ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 // Если в строке нет ни одной цифры, функция должна вернуть NaN
-let extractNumbers = (string) => {
-  let numbers = '0123456789'.split('');
+const extractNumbers = (string) => {
+  const numbers = '0123456789'.split('');
   let newString = '';
 
   for (let i = 0; i < String(string).length; i++) {
     for (let j = 0; j < numbers.length; j++) {
-      if (String(string)[i] == numbers[j]) {
-        newString+=String(string)[i];
+      if (String(string)[i] === numbers[j]) {
+        newString += String(string)[i];
       }
     }
   }
-  if (newString != '') {
+  if (newString !== '') {
     return newString;
   }
   return NaN;
-}
+};
 
 //Проверка
 console.log(extractNumbers('2023 год'));
