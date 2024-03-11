a9572e08a6bfc71065fd636cd53d436359c23384
lisakolts <crokodile.liza@gmail.com>
2024-03-11 14:14:32 +0300

    Открывается и закрывается (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-import {} from './draw_miniatures.js';
-import './open_close_picture_popup.js';
+import { createPosts } from './create-post.js';
+import { drawMiniatures } from './draw-miniatures.js';
+import './picture-popup.js';
 
+const miniatures = createPosts();
+drawMiniatures(miniatures);
+
+export { miniatures };
