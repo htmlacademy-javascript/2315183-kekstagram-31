41d0ee18b023c38334604b56c72fda3b9a7d5a7f
Keks <keks@htmlacademy.ru>
2024-03-11 14:16:14 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,8 @@
+import { createPosts } from './create-post.js';
+import { drawMiniatures } from './draw-miniatures.js';
+import './picture-popup.js';
+
+const miniatures = createPosts();
+drawMiniatures(miniatures);
+
+export { miniatures };
