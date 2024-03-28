3380e2b742c4aca653be9b4c043be971682d6610
lisakolts <crokodile.liza@gmail.com>
2024-03-28 15:11:18 +0300

    Фото на память

--- js/picture-form-popup.js
+++ js/picture-form-popup.js
@@ -1,13 +1,18 @@
-import { addModalOpen, isEscapeKey } from './utils.js';
+import { addModalOpen, createElement, isEscapeKey } from './utils.js';
 import { onPictureBigger, onPictureSmaller, resetImageScale } from './picture_scale.js';
 import { changeImageEffect, clearEffects, createSlider } from './picture-filter.js';
 import { checkForm } from './form-validation.js';
+
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const loadImageFormPopup = document.querySelector('.img-upload__overlay');
 const loadImageFormPopupOpen = document.querySelector('.img-upload__input');
 const loadImageFormPopupClose = loadImageFormPopup.querySelector('.img-upload__cancel');
 
 const loadImageForm = document.querySelector('.img-upload__form');
+
+const imagePreview = loadImageForm.querySelector('.img-upload__preview img');
+const imageEffectPreview = loadImageForm.querySelectorAll('.effects__preview');
 
 const hashtagInput = loadImageFormPopup.querySelector('.text__hashtags');
 const commentInput = loadImageFormPopup.querySelector('.text__description');
@@ -16,6 +21,8 @@ const scaleSmallerButton = loadImageFormPopup.querySelector('.scale__control--sm
 const scaleBiggerButton = loadImageFormPopup.querySelector('.scale__control--bigger');
 
 const effectRadioButton = document.querySelector('.effects__list');
+
+let onDocumentKeydown = () => {};
 
 const onAddEffects = () => {
   const checkedButton = effectRadioButton.querySelector('input[name="effect"]:checked').value;
@@ -30,11 +37,25 @@ const clearForm = () => {
   onAddEffects();
 };
 
-let onDocumentKeydown = () => {};
+const setPersonalImage = () => {
+  const image = loadImageFormPopupOpen.files[0];
+  const imageName = image.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => imageName.endsWith(it));
+
+  if (matches) {
+    imagePreview.src = URL.createObjectURL(image);
+
+    imageEffectPreview.forEach((item) => {
+      item.style.backgroundImage = `url("${imagePreview.src}")`;
+    });
+  }
+};
 
 const openLoadImageForm = () => {
   clearForm();
   addModalOpen();
+  setPersonalImage();
 
   loadImageFormPopup.classList.remove('hidden');
 
